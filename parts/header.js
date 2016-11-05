35e03a59dbfbc66f0e6d0fa67af1cd8fad351eaf
DragonFerocity <rtanq9@mst.edu>
2016-11-05 17:21:19 -0500

    Fixed header stuff

--- parts/header.js
+++ parts/header.js
@@ -16,13 +16,13 @@ $(function() {
     var scroll = $(document).scrollTop();
     if ($(".menu").length > 0) {
       if (scroll >= pos) {
-        $(".header-text").css({"position": "fixed", "top": "70px"});
-        $(".menu").css({"position": "fixed", "top": "0px"});
+        $(".header-text").css({"position": "fixed", "top": "0px"});
+        $(".menu").css({"position": "fixed", "top": "48px"});
         $("#menubar").animate({"background-color": "transparent"}, 500);
       }
       else {
         $(".header-text").css({"position": "relative", "top": "0px"});
-        $(".menu").css({"position": "fixed", "top": "0px"});
+        $(".menu").css({"position": "relative", "top": "0px"});
         $("#menubar").animate({"background-color": "rgba(32, 32, 32, 0.8)"}, 500);
       }
     }

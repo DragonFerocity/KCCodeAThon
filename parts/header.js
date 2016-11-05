810d2a5bb92db7151c762fb54f7a53c32152c98b
DragonFerocity <rtanq9@mst.edu>
2016-11-05 12:40:57 -0500

    organized and updated some styles

--- parts/header.js
+++ parts/header.js
@@ -7,22 +7,6 @@ $(function() {
       $("#menubar").append("<div id='linkbar'></div>");
       $("#linkbar").append("<a href=\"\"><div class=\"menulink\">Home</div></a>");
       $("#linkbar").append("<a href=\"\"><div class=\"menulink\">Location</div></a>");
-      $("body").append("<section id=\"content\"></section>");
-  		/*var linkBar = document.getElementById("linkbar");
-
-  		var newLink = document.createElement("a");
-  		var newLinkDiv = document.createElement("div");
-  		newLink.class = "menulink";
-  		newLink.href = "http://www.google.com";
-  		newLinkDiv.class = "menulink";
-  		newLinkDiv.innerHTML = "Home";
-  		newLink.append(newLinkDiv);
-
-  		//var link = document.createElement("img");
-  		//link.src = "http://wardencoding.x10host.com/files/home-default.png"
-  		//link.onmouseover = function(){this.src = 'http://wardencoding.x10host.com/files/home-hover.png'};
-  		//link.onouseout = function(){this.src = 'http://wardencoding.x10host.com/files/home-default.png'};
-  		linkBar.append(newLink);*/
   	}
   }
 

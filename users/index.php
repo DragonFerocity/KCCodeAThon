c4d8a529cc903976ed441325d3594eb3fca22cb8
Jumperxd <ceg8b4@mst.edu>
2016-11-05 20:45:32 -0500

    Major Changes In Structure Of User Profile Page

--- users/index.php
+++ users/index.php
@@ -14,78 +14,88 @@
         <div class="col-md-4 col-xs-4">
           <img src = "https://ih1.redbubble.net/image.214499180.8793/flat,800x800,075,f.jpg" class = "img-square profile_pic" alt = "dat boi">
         </div>
-      </div>
-    </div>
-    <div class = "container">
-      <div class = "row">
-        <div class = "col-md-offset-6 col-md-2 col-xs-offset-6 col-xs-2">
+        <div class = "col-md-offset-10 col-md-2 col-xs-offset-10 col-xs-2">
           <a href = "user_edit_profile.html" type = "button" class = "btn btn-primary">
             <span class = "glyphicon glyphicon-pencil"></span>
             Edit Profile
           </a>
         </div>
       </div>
-      <div class = "row row_size">
-        <div class = "col-md-12"><h2>My Info</h2></div>
-      </div>
-      <div class = "row row_size text-left">
-        <div class = "col-md-2 static_list">First Name:</div>
-        <div class = "col-md-2">Dat</div>
-        <div class = "col-md-2 static_list">Last Name:</div>
-        <div class = "col-md-2">Boi</div>
-        <div class = "col-md-2 static_list">Location:</div>
-        <div class = "col-md-2">Rolla MO</div>
-      </div>
-      <div class = "row text-left">
-        <div class = "col-md-2 static_list">Age:</div>
-        <div class = "col-md-2">9000</div>
-        <div class = "col-md-2 static_list">Gender:</div>
-        <div class = "col-md-2">Boi</div>
-        <div class = "col-md-2 static_list">Email:</div>
-        <div class = "col-md-2">datboi@meme.edu</div>
-      </div>
-
-      <div class = "row row_size">
-        <div class = "col-md-12"><h2>My Availability</h2></div>
-      </div>
-      <div class = "row row_size">
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1">Monday</div>
-        <div class = "col-md-1">Tuesday</div>
-        <div class = "col-md-1">Wednesday</div>
-        <div class = "col-md-1">Thursday</div>
-        <div class = "col-md-1">Friday</div>
-        <div class = "col-md-1">Saturday</div>
-        <div class = "col-md-1">Sunday</div>
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1"><!--empty--></div>
+      <div class = "row">
+        <div class = "col-md-offset-4 col-xs-offset-4 col-md-8 col-xs-8">
+          <h2>My Info</h2>
+        </div>
       </div>
       <div class = "row">
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1 time">8:00 - 18:00</div>
-        <div class = "col-md-1 time">3:12 - 14:23</div>
-        <div class = "col-md-1 time">12:03 - 20:22</div>
-        <div class = "col-md-1 time">13:12 - 13:13</div>
-        <div class = "col-md-1 time">N/A</div>
-        <div class = "col-md-1 time">N/A</div>
-        <div class = "col-md-1 time">N/A</div>
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1"><!--empty--></div>
-        <div class = "col-md-1"><!--empty--></div>
+        <div class = "col-md-offset-4 col-md-2 col-xs-offset-4 col-xs-2 static_list">
+          <p>First Name:</p>
+        </div>
+        <div class = "col-md-2 col-xs-2"><p>Dat</p></div>
+        <div class = "col-md-2 col-xs-2 static_list"><p>Last Name:</p></div>
+        <div class = "col-md-2 col-xs-2"><p>Boi</p></div>
       </div>
-      <div class = "row row_size">
-        <div class = "col-md-12"><h2>My Skills</h2></div>
+      <div class="row">
+        <div class = "col-md-offset-4 col-md-2 col-xs-offset-4 col-xs-2 static_list">
+          <p>Location:</p>
+        </div>
+        <div class = "col-md-2 col-xs-2"><p>Rolla MO</p></div>
+        <div class = "col-md-2 col-xs-2 static_list"><p>Age:</p></div>
+        <div class = "col-md-2 col-xs-2"><p>9000</p></div>
       </div>
-      <div class = "row row_size">
-        <div class = "col-md-2">good at doing stuff</div>
-        <div class = "col-md-2">best chess player</div>
-        <div class = "col-md-2">dabbing</div>
-        <div class = "col-md-2">mad skillz</div>
-        <div class = "col-md-2">pro DJ</div>
-        <div class = "col-md-2">swagger</div>
+      <div class="row">
+        <div class = "col-md-offset-4 col-md-2 col-xs-offset-4 col-xs-2 static_list">
+          <p>Gender:</p>
+        </div>
+        <div class = "col-md-2 col-xs-2"><p>Boi</p></div>
+        <div class = "col-md-2 col-xs-2 static_list"><p>Email:</p></div>
+        <div class = "col-md-2 col-xs-2"><p>datboi@meme.edu</p></div>
+      </div>
+
+      <div class = "row">
+        <div class = "col-md-offset-4 col-md-8 col-xs-offset-4 col-xs-8">
+          <h2>My Availability</h2>
+        </div>
+      </div>
+      <div class = "row">
+        <div class = "col-md-offset-4 col-md-8 col-xs-offset-4 col-xs-8">
+          <table>
+            <tr>
+              <td>Monday</td>
+              <td>Tuesday</td>
+              <td>Wednesday</td>
+              <td>Thursday</td>
+              <td>Friday</td>
+              <td>Satday</td>
+              <td>Sunday</td>
+            </tr>
+            <tr>
+              <td>Availability Put Here</td>
+              <td>Availability Put Here</td>
+              <td>Availability Put Here</td>
+              <td>Availability Put Here</td>
+              <td>Availability Put Here</td>
+              <td>Availability Put Here</td>
+              <td>Availability Put Here</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div class = "row">
+        <div class = "col-md-offset-4 col-md-8 col-xs-offset-4 col-xs-4">
+          <h2>My Skills</h2>
+        </div>
+      </div>
+      <div class = "row">
+        <div class = "col-md-offset-4 col-md-8 col-xs-offset-4 col-xs-8">
+          <ol>
+            <li>Place Skill Here.</li>
+            <li>Place Skill Here.</li>
+            <li>Place Skill Here.</li>
+            <li>Place Skill Here.</li>
+            <li>Place Skill Here.</li>
+            <li>Place Skill Here.</li>
+          </ol>
+        </div>
       </div>
     </div>
   </section>

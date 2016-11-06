6058a96318f8e14d7f1d93e569c97c84bfab4ed0
DragonFerocity <rtanq9@mst.edu>
2016-11-06 15:15:36 -0600

    ...

--- login/sign-up.php
+++ login/sign-up.php
@@ -2,14 +2,16 @@
 <html lang = "en">
 <head>
   <title>KC Code-A-Thon</title>
-  <?php include("../parts/header.php?2"); ?>
+  <?php
+  include "../parts/header.php";
+  ?>
 </head>
 <body>
   <section id="content-top-margin">
-    <section id="account-type" class="table-outer">
+    <section id="account-type" class="table-outer center-text" style="width: 60%;">
       <h2>Choose your account type</h2>
-      <button class="btn btn-primary btn-lg" type="button">Volunteer</button>
-      <button class="btn btn-primary btn-lg" type="button">Organization</button>
+      <button class="btn btn-primary btn-md" type="button">Volunteer</button>
+      <button class="btn btn-primary btn-md" type="button">Organization</button>
     </section>
   </section>
 </body>

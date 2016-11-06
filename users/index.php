d60b1bc9a226ace65d961d63153bc5a6c90ca591
Jumperxd <ceg8b4@mst.edu>
2016-11-05 21:39:34 -0500

    changed styling

--- users/index.php
+++ users/index.php
@@ -36,6 +36,10 @@
         <div class = "col-md-2 col-xs-2"><p>Rolla MO</p></div>
         <div class = "col-md-2 col-xs-2 static_list"><p>Age:</p></div>
         <div class = "col-md-2 col-xs-2"><p>9000</p></div>
+        <div class="col-md-4 col-xs-4">
+          <img src = "https://ih1.redbubble.net/image.214499180.8793/flat,800x800,075,f.jpg" 
+          class = "img-square profile_pic" alt = "dat boi" id="pic">
+        </div>
       </div>
       <div class="row">
         <div class = "col-md-offset-4 col-md-2 col-xs-offset-4 col-xs-2 static_list">
@@ -44,12 +48,6 @@
         <div class = "col-md-2 col-xs-2"><p>Boi</p></div>
         <div class = "col-md-2 col-xs-2 static_list"><p>Email:</p></div>
         <div class = "col-md-2 col-xs-2"><p>datboi@meme.edu</p></div>
-      </div>
-      <div class="row">
-        <div class="col-md-4 col-xs-4">
-          <img src = "https://ih1.redbubble.net/image.214499180.8793/flat,800x800,075,f.jpg" 
-          class = "img-square profile_pic" alt = "dat boi" id="pic">
-        </div>
       </div>
       <div class = "row">
         <div class = "col-md-offset-4 col-md-8 col-xs-offset-4 col-xs-8">
